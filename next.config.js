1fcb35911f34dfacde97b5a05f01dadce26c49be
Rishav Raj <rishavraj@Rishavs-MacBook-Air.local>
2022-10-28 12:47:02 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
-};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig;
